4d5e9e01bb3f5d0972bd59fa6b1244fdae0bffdd
Thejus Rajendran <63202440+thejus-r@users.noreply.github.com>
2022-07-11 19:54:08 +0530

    Update AuthorModel.js

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Library", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/Library", {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
